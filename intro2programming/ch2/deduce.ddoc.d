43aae76ae48ba607899c792db50eb340c52886b7
curoles <curoles@yahoo.com>
2015-09-11 20:15:54 -0700

    i2p ch3

--- intro2programming/ch2/deduce.ddoc.d
+++ intro2programming/ch2/deduce.ddoc.d
@@ -1,1 +1,49 @@
 $(H2 Type Auto Deducing)
+
+$(P
+Computer languages have different approach to
+how important it is to explicitly specify type of a variable.
+Some languages go to the extream and
+do not even give a programmer a way to specify
+types, those languages figure out types themself.
+It can be conviniet for a programmer not to bother with types,
+but there are situations when programmer would like to know
+what kind of types some code is dealing with and this information
+is not available, it makes reasoning about the code
+difficult in some cases.
+)
+
+$(P
+D does not go to that extrem.
+It allows programmer to specify variable's type but
+it also can automatically deduce variable's type,
+in case programmer wants it.
+In many cases it actually makes sense to NOT assume a particular
+type and let compiler to figure out it,
+it makes more generic code.
+)
+
+$(P
+In case you want compiler to figure out type in your behalf
+replace actual type with keyword `auto`.
+
+Let us consider an example.
+---
+import std.stdio;
+
+auto i = 5;
+auto f = 3.14;
+
+void main()
+{
+  writefln("i is %s", typeid(typeof(i)));
+  writefln("f is %s", typeid(typeof(f)));
+}
+---
+
+Application output:
+$(PRE
+i is int
+f is double
+)
+)
--- intro2programming/ch3/ch3.ddoc.d
+++ intro2programming/ch3/ch3.ddoc.d
@@ -1,10 +1,46 @@
 $(H1 Chapter 3. Execution Flow)
 
 $(P
-5things!
-only if and loop 
+This chapter is mainly about two things computers can do:
+$(UL 
+$(LI
+The Thing #2: Choose between options and branch execution accordingly.
+)
+$(LI
+The Thing #3: Stay in a loop and repeat something.
+)
+)
 )
 
 $(P
-p2
+An ability to make choises is perhaps the most
+important part of any intellegence.
+Even simplest living organizms make their choices
+in order to survive and grow.
+But what does it mean for a computer to chose?
+For a computer act of chosing has two phases:
+$(OL
+$(LI
+Make calculations that result in a boolean value
+that can be $(B true) or $(B false).
+)
+$(LI
+Based on the result from phase 1, jump to execution of
+one piece of code or another.
+)
+)
+)
+
+$(P
+Everything computer does is kept in its memory.
+One computer instruction follow another instruction
+back-to-back in the memory. Because of that
+computer after he executed currect instruction
+can just fetch next instruction from the memory
+and execute it and so on. But this way computer
+execution flow is always the same, linear.
+)
+
+$(P
+Now image there is an instruction like "jump 10 instructions forward".
 )
