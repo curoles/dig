17434846e968da265bb8402850f4fea57da14774
curoles <curoles@yahoo.com>
2015-09-15 09:19:20 -0700

    i2p

--- intro2programming/ch4/define.ddoc.d
+++ intro2programming/ch4/define.ddoc.d
@@ -6,4 +6,23 @@ Here is how a function is defined in D:
 // ReturnValue FunctionName( Arguments ) { Statements; }
       int           sum   (int a, int b) { return a + b; }
 ---
+
+
+$(UL
+$(LI
+First goes function return value type, it can be any type or
+special type `void` if a function returns nothing.
+)
+$(LI
+Then comes name of the function.
+)
+$(LI
+Then in parenthesis go function arguments.
+)
+$(LI
+Then inside the curly brackets body of the function,
+that is what code statements implementing what the function does.
+)
+)
+
 )
